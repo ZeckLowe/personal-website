a8b3ef323c619ac8d3ca08f87a08fcd9050079c5
Czech Louie <czech.sadiangabay@amcsgroup.com>
2025-03-13 20:01:08 +0800

    added assets and created a navbar

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,16 +1,8 @@
 import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
-  return (
-    <div>
-      <div className="container bg-blue-500 mx-auto">
-        <h1 className="text-green-50">Hello tailwindcssv4 react</h1>
-        <div>
-          <h2 className="text-2xl">This is 2xl</h2>
-        </div>
-      </div>
-    </div>
-  );
+  return <Header />;
 };
 
 export default App;
